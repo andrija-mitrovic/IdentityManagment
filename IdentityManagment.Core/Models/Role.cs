4fe410f415fffde448da8dd7bd645c20df174b7b
andrija-mitrovic <57568556+andrija-mitrovic@users.noreply.github.com>
2020-08-30 12:18:03 +0200

    Initial Commit

---
+++ IdentityManagment.Core/Models/Role.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IdentityManagment.Core.Models
+{
+    public class Role:IdentityRole<int>
+    {
+        public ICollection<UserRole> UserRoles { get; set; }
+    }
+}
---
+++ IdentityManagment.Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IdentityManagment.Infrastructure.Data
+{
+    public class ApplicationDbContext:IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options):base(options) { }
+
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
