1efa46fced64cb63d61054b53ce53b3e7c5acd4d
andrija-mitrovic <57568556+andrija-mitrovic@users.noreply.github.com>
2020-09-01 10:37:45 +0200

    EmployeeController added

---
+++ IdentityManagment.WebAPI/Controllers/EmployeeController.cs
@@ -0,0 +1,98 @@
+﻿using AutoMapper;
+using IdentityManagment.Core.DTOs;
+using IdentityManagment.Core.Interfaces;
+using IdentityManagment.Core.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IdentityManagment.WebAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EmployeeController:ControllerBase
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IMapper _mapper;
+
+        public EmployeeController(IUnitOfWork unitOfWork, IMapper mapper)
+        {
+            _unitOfWork = unitOfWork;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetEmployees()
+        {
+            var employees = await _unitOfWork.Employees.GetAllAsync();
+            
+            return Ok(employees);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetEmployee(int id)
+        {
+            var employee = await _unitOfWork.Employees.GetByIdAsync(id);
+
+            if(employee==null)
+            {
+                return NotFound();
+            }
+
+            return Ok(employee);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateEmployee(EmployeeDto employeeDto)
+        {
+            var employee = _mapper.Map<Employee>(employeeDto);
+
+            await _unitOfWork.Employees.AddAsync(employee);
+
+            if(await _unitOfWork.SaveAsync())
+            {
+                return StatusCode(201);
+            }
+
+            throw new Exception("Creating employee failed on save");
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateEmployee(int id, EmployeeDto employeeDto)
+        {
+            var employee = await _unitOfWork.Employees.GetByIdAsync(id);
+
+            if (employee == null)
+            {
+                return NotFound();
+            }
+
+            _mapper.Map(employeeDto, employee);
+            _unitOfWork.Employees.Update(employee);
+
+            if(await _unitOfWork.SaveAsync())
+            {
+                return NoContent();
+            }
+
+            throw new Exception($"Updating user {id} failed on save");
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteEmployee(int id)
+        {
+            var employee = await _unitOfWork.Employees.GetByIdAsync(id);
+
+            _unitOfWork.Employees.Remove(employee);
+
+            if(await _unitOfWork.SaveAsync())
+            {
+                return NoContent();
+            }
+
+            throw new Exception("Error deleting employee");
+        }
+    }
+}
