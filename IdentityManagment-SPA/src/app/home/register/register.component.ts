83bfad69b960c660e80060824e50ca12ead08fbd
andrija-mitrovic <57568556+andrija-mitrovic@users.noreply.github.com>
2020-09-13 08:52:14 +0200

    User registration added

--- IdentityManagment-SPA/src/app/home/register/register.component.ts
+++ IdentityManagment-SPA/src/app/home/register/register.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit, EventEmitter, Output } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { User } from 'src/app/_models/user';
+import { AlertifyService } from 'src/app/_services/alertify.service';
+import { AuthService } from 'src/app/_services/auth.service';
 
 @Component({
   selector: 'app-register',
@@ -8,14 +12,47 @@ import { FormGroup } from '@angular/forms';
 })
 export class RegisterComponent implements OnInit {
   @Output() cancelRegister = new EventEmitter();
+  user: User;
   registerForm: FormGroup;
-  constructor() { }
+  constructor(private fb: FormBuilder,
+              private auth: AuthService,
+              private alertify: AlertifyService,
+              private router: Router) { }
 
   ngOnInit() {
+    this.createRegisterForm();
+  }
+
+  createRegisterForm() {
+    this.registerForm = this.fb.group({
+      gender: ['male'],
+      username: ['', Validators.required],
+      knownAs: ['', Validators.required],
+      dateOfBirth: ['', Validators.required],
+      country: ['', Validators.required],
+      city: ['', Validators.required],
+      password: ['', [Validators.required, Validators.minLength(4), Validators.maxLength(8)]],
+      confirmPassword: ['', Validators.required]
+    }, { validator: this.passwordMatchValidator});
+  }
+
+  passwordMatchValidator(g: FormGroup) {
+    return g.get('password').value === g.get('confirmPassword').value ? null : {'mismatch': true};
   }
 
   register() {
-
+    if(this.registerForm.valid) {
+      this.user = Object.assign({}, this.registerForm.value);
+      this.auth.register(this.user).subscribe(() => {
+        this.alertify.success('Registration successful');
+      }, error => {
+        this.alertify.error(error);
+      },() => {
+        this.auth.login(this.user).subscribe(() => {
+          this.router.navigate(['/employees']);
+        });
+      });
+    }
   }
 
   cancel() {
